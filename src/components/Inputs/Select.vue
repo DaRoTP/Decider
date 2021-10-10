651d4c9d47d51414fc2883b7d3d020295a420f7c
Darek <DRodzewicz@gmail.com>
2021-10-10 17:11:22 +0200

    [ADD] #6 Add Select component

---
+++ src/components/Inputs/Select.vue
@@ -0,0 +1,36 @@
+<template>
+  <InputGroup :label="label">
+    <template #input>
+      <select class="shadow-md rounded-sm p-2">
+        <option v-for="option in options" :key="option" :value="option">
+          {{ option }}
+        </option>
+      </select>
+    </template>
+  </InputGroup>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { ComonInputProps } from "./props";
+import InputGroup from "./InputGroup.vue";
+
+export default defineComponent({
+  name: "Select",
+  components: {
+    InputGroup,
+  },
+  props: {
+    ...ComonInputProps,
+    options: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
