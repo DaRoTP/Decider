4a645eebac6c90c57efded91feca5581d5c545e5
Darek <DRodzewicz@gmail.com>
2021-10-10 17:51:34 +0200

    [ADD] #6 Add a TextArea component

---
+++ src/components/Inputs/TextArea.vue
@@ -0,0 +1,33 @@
+<template>
+  <InputGroup
+    :label="label"
+    :subText="error"
+    class="input"
+    :class="{ 'in-valid': !error }"
+  >
+    <template #input>
+      <textarea class="bg-white shadow-md rounded-sm p-2 resize-none" />
+    </template>
+  </InputGroup>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import InputGroup from "./InputGroup.vue";
+import { ComonInputProps } from "./props";
+
+export default defineComponent({
+  name: "TextArea",
+  components: {
+    InputGroup,
+  },
+  props: {
+    ...ComonInputProps,
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
