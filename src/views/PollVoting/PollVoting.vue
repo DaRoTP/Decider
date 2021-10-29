18f0084537ccbd06fdc6a3f09bebca2bada98ff0
Darek <DRodzewicz@gmail.com>
2021-10-30 00:03:12 +0200

    [FEAT] #22 Spinner

--- src/views/PollVoting/PollVoting.vue
+++ src/views/PollVoting/PollVoting.vue
@@ -2,16 +2,19 @@
   <div class="flex flex-col">
     <div class="flex justify-between">
       <h1 class="text-primary font-bold text-xl">{{ title }}</h1>
-      <Timer :endDate="endDate" />
+      <Timer v-if="!isLoading" :endDate="endDate" />
     </div>
     <hr class="my-2" />
-    <PollStart />
-    <component :is="PollVotingComponent" />
+    <GridSpinner v-if="isLoading" />
+    <template v-else>
+      <PollStart />
+      <component :is="PollVotingComponent" />
+    </template>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, computed } from "vue";
+import { defineComponent, ref, computed, onMounted } from "vue";
 import { viewNames } from "@/router/views";
 import { PollType } from "@/types";
 import Timer from "@/components/Timer.vue";
@@ -19,6 +22,7 @@ import PollVotingSelect from "./PollVotingSelect.vue";
 import PollVotingMeter from "./PollVotingMeter.vue";
 import PollVotingBinary from "./PollVotingBinary.vue";
 import PollStart from "./PollStart.vue";
+import GridSpinner from "@/components/Spinners/GridSpinner.vue";
 
 export default defineComponent({
   name: viewNames.POLL_VOTING,
@@ -28,6 +32,7 @@ export default defineComponent({
     PollVotingBinary,
     PollStart,
     Timer,
+    GridSpinner,
   },
   props: {
     pollId: {
@@ -35,9 +40,28 @@ export default defineComponent({
     },
   },
   setup() {
-    const title = ref<string>("Test title");
-    const type = ref<PollType>(PollType.BINARY);
-    const endDate = ref<Date>(new Date("2021-10-29T20:18"));
+    const data = {
+      title: "Some poll that I don't care about",
+      descirpion:
+        "this is some randome lorem ipsum that I also don't really care about ok thats it about this description or about this poll",
+      endDate: "2021-10-29T20:18",
+      resultPreview: true,
+      userVoted: false,
+      type: PollType.BINARY,
+    };
+
+    const title = ref<string>("");
+    const type = ref<PollType>();
+    const endDate = ref<Date>();
+
+    const isLoading = ref<boolean>(true);
+
+    onMounted(() => {
+      title.value = data.title;
+      type.value = data.type;
+      endDate.value = new Date(data.endDate);
+      isLoading.value = false;
+    });
 
     const PollVotingComponent = computed(() => {
       switch (type.value) {
@@ -55,6 +79,7 @@ export default defineComponent({
       title,
       PollVotingComponent,
       endDate,
+      isLoading,
     };
   },
 });
