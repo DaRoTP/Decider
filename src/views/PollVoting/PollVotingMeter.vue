5e30c13af1b97019cda918b6cec0209d1e52a9f4
Darek <DRodzewicz@gmail.com>
2021-10-29 16:10:55 +0200

    [FIX] #18 alignement fix

--- src/views/PollVoting/PollVotingMeter.vue
+++ src/views/PollVoting/PollVotingMeter.vue
@@ -1,15 +1,17 @@
 <template>
-  <div>
+  <div class="flex flex-col">
     <PollSteps
-      class="my-4"
+      class="my-4 self-center"
       :numberOfSteps="3"
       v-model:currentStep="currentStep"
       :checkedSteps="checkedSteps"
       stepNavType="BACK"
     />
     <template v-if="options.length > 0">
-      <h1>{{ options[currentStep - 1].name }}</h1>
-      <p>{{ options[currentStep - 1].description }}</p>
+      <h1 class="text-primary font-bold text-xl">
+        {{ options[currentStep - 1].name }}
+      </h1>
+      <p class="text-primary">{{ options[currentStep - 1].description }}</p>
       <div class="flex gap-2">
         <div class="flex gap-1 items-center">
           <Input type="number" v-model="meterValue[currentStep - 1]" />
@@ -25,14 +27,14 @@
       <button
         v-if="currentStep === options.length"
         @click="submitHandler"
-        class="btn-primary p-2 rounded-full px-6 my-4"
+        class="btn-primary p-2 rounded-full px-6 my-4 self-end"
       >
         Submit
       </button>
       <button
         v-else
         @click="nextOptionHandler"
-        class="btn-primary p-2 rounded-full px-6 my-4"
+        class="btn-primary p-2 rounded-full px-6 my-4 self-end"
       >
         Next
       </button>
