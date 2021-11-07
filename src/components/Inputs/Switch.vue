77346f44921e2f170486fa03f737ee7f8c267272
Darek <DRodzewicz@gmail.com>
2021-11-07 21:47:04 +0100

    [ADD] #30 Added InputGroup to Switch component

--- src/components/Inputs/Switch.vue
+++ src/components/Inputs/Switch.vue
@@ -1,39 +1,51 @@
 <template>
-  <button
-    @click="$emit('update:modelValue', !modelValue)"
-    :style="{ width: '4rem', height: '2rem' }"
-    class="bg-primary rounded-md p-1 relative"
-    :class="{ 'bg-gray-400': !modelValue }"
+  <InputGroup
+    :label="label"
+    :subText="error"
+    class="app-input"
+    :class="{ 'in-valid': !error }"
   >
-    <span
-      :style="{
-        width: '1.8rem',
-        height: '1.4rem',
-        transform: modelValue ? 'translate(1.6rem)' : '',
-      }"
-      class="
-        bg-white
-        rounded-sm
-        shadow-sm
-        grid
-        place-items-center
-        transition-all
-      "
-    >
-      <fa v-if="modelValue" icon="check" class="text-primary text-xs" />
-    </span>
-  </button>
+    <template #input>
+      <button
+        @click="$emit('update:modelValue', !modelValue)"
+        :style="{ width: '4rem', height: '2rem' }"
+        class="bg-primary rounded-md p-1 relative"
+        :class="{ 'bg-gray-400': !modelValue }"
+      >
+        <span
+          :style="{
+            width: '1.8rem',
+            height: '1.4rem',
+            transform: modelValue ? 'translate(1.6rem)' : '',
+          }"
+          class="
+            bg-white
+            rounded-sm
+            shadow-sm
+            grid
+            place-items-center
+            transition-all
+          "
+        >
+          <fa v-if="modelValue" icon="check" class="text-primary text-xs" />
+        </span>
+      </button>
+    </template>
+  </InputGroup>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import InputGroup from "./InputGroup.vue";
+import { ComonInputProps } from "./props";
 
 export default defineComponent({
   name: "Switch",
+  components: {
+    InputGroup,
+  },
   props: {
-    modelValue: {
-      type: Boolean,
-    },
+    ...ComonInputProps,
   },
   setup() {
     return {};
