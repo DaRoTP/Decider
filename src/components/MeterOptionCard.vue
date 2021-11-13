3e47e4e603d540e1ba0847021357abca86c503a3
Darius Rodzevic <35233347+DaRoTP@users.noreply.github.com>
2021-11-13 18:26:37 +0100

    Meter voting (#39)
    * [MODIFY] #38 modified meter voting panel view
    
    * [ADD] #38 added meter option card component
    
    * [FIX #38 fixed meter voting component aligment
    
    * [ADD] #38 MeterOptionCard animation - added animation to MeterOptionCard on first render
    
    * [MODIFY] #38 modified meter option card size

---
+++ src/components/MeterOptionCard.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="meter-option-card">
+    <img
+      class="object-cover shadow-md rounded-md"
+      :src="imageSrc"
+      alt="option"
+    />
+    <div class="card-1 shadow-md rounded-md"></div>
+    <div class="card-2 shadow-md rounded-md"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MeterOptionCard",
+  props: {
+    imageSrc: {
+      type: String,
+      default: "",
+    },
+  },
+});
+</script>
+
+<style lang="scss">
+$card-width: 20rem;
+$card-height: 20rem;
+
+@mixin card-slide($name, $translate-x, $rotate) {
+  @keyframes #{$name} {
+    from {
+      transform: translateX(0%);
+    }
+    to {
+      transform: translateX($translate-x) rotate($rotate);
+    }
+  }
+}
+
+@include card-slide(image-slide, -4rem, -10deg);
+@include card-slide(card-1-slide, -2rem, -2deg);
+@include card-slide(card-2-slide, 0, 4deg);
+
+.meter-option-card {
+  position: relative;
+  width: $card-width + 6.5rem;
+  height: $card-height + 4rem;
+
+  %card {
+    top: 2rem;
+    right: 1rem;
+    width: $card-width;
+    height: $card-height;
+    position: absolute;
+  }
+  img {
+    @extend %card;
+    z-index: 3;
+    animation: image-slide 0.5s forwards 0.1s;
+  }
+  .card-1 {
+    @extend %card;
+    z-index: 2;
+    background: rgb(223, 225, 247);
+    animation: card-1-slide 0.5s forwards 0.1s;
+  }
+  .card-2 {
+    @extend %card;
+    z-index: 1;
+    background: rgb(176, 180, 231);
+    animation: card-2-slide 0.5s forwards 0.1s;
+  }
+}
+</style>
--- src/views/MeterPoll/MeterPollVotingPanel.vue
+++ src/views/MeterPoll/MeterPollVotingPanel.vue
@@ -1,37 +1,50 @@
 <template>
-  <div class="flex flex-col">
-    <h1 class="text-primary font-bold text-xl">
-      {{ options[currentStep - 1].name }}
-    </h1>
-    <p class="text-primary">{{ options[currentStep - 1].description }}</p>
-    <div class="flex gap-2">
-      <div class="flex gap-1 items-center">
-        <Input type="number" v-model="meterValue[currentStep - 1]" />
-        <span>%</span>
+  <div class="flex flex-col items-center">
+    <PollSteps
+      :numberOfSteps="options.length"
+      :currentStep="currentStep"
+      :checkedSteps="checkedSteps"
+      stepNavType="BACK"
+    />
+    <div class="flex flex-col mt-4">
+      <MeterOptionCard v-if="show" :imageSrc="currentOption.imageSrc" />
+      <div class="my-3">
+        <h1 class="text-primary font-bold text-xl text-center">
+          {{ currentOption.name }}
+        </h1>
+        <p class="text-primary text-center">{{ currentOption.description }}</p>
       </div>
-      <Slider
-        class="flex-grow"
-        v-model="meterValue[currentStep - 1]"
-        :min="0"
-        :max="100"
-      />
+      <div class="flex gap-2">
+        <div class="flex gap-1 items-center">
+          <Input type="number" v-model="meterValue" />
+          <span>%</span>
+        </div>
+        <Slider class="flex-grow" v-model="meterValue" :min="1" :max="100" />
+      </div>
+      <button
+        v-if="meterValue !== 0"
+        @click="nextStepHandler"
+        class="btn-primary p-2 rounded-md px-6 my-4 self-end"
+      >
+        {{ currentStep >= options.length ? "Confirm" : "Next" }}
+      </button>
     </div>
-    <button
-      @click="nextStepHandler"
-      class="btn-primary p-2 rounded-full px-6 my-4 self-end"
-    >
-      Next
-    </button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, toRef, PropType } from "vue";
+import { defineComponent, computed, ref, PropType } from "vue";
 import { IOption } from "@/types";
 import { Views } from "@/router/viewNames";
+import PollSteps from "@/components/PollSteps.vue";
+import MeterOptionCard from "@/components/MeterOptionCard.vue";
 
 export default defineComponent({
   name: Views.VOTING_PANNEL.METER,
+  components: {
+    PollSteps,
+    MeterOptionCard,
+  },
   props: {
     options: {
       type: Array as PropType<IOption[]>,
@@ -41,25 +54,43 @@ export default defineComponent({
       type: Array as PropType<number[]>,
       required: true,
     },
-    currentStep: {
-      type: Number,
-      default: 1,
-    },
   },
   setup(props, { emit }) {
-    const meterValue = toRef(props, "submittingData");
-    emit(
-      "update:submittingData",
-      props.options.map(() => 0)
+    const currentStep = ref<number>(1);
+    const meterValue = ref<number>(0);
+    const show = ref<boolean>(true);
+
+    const checkedSteps = computed(() =>
+      Object.values(props.submittingData)
+        .map((val, index) => (val <= 0 ? -1 : index + 1))
+        .filter((item) => item > -1)
     );
 
+    const currentOption = computed(() => props.options[currentStep.value - 1]);
+
     const nextStepHandler = () => {
-      emit("change:step", props.currentStep + 1);
-      emit("update:submittingData", meterValue.value);
+      emit("update:submittingData", {
+        ...props.submittingData,
+        [currentOption.value.name]: meterValue.value,
+      });
+      show.value = false;
+      setTimeout(() => {
+        show.value = true;
+      }, 500);
+      if (currentStep.value >= props.options.length) {
+        emit("confirm");
+      }
+
+      currentStep.value += 1;
+      meterValue.value = 0;
     };
 
     return {
       meterValue,
+      currentStep,
+      checkedSteps,
+      currentOption,
+      show,
       nextStepHandler,
     };
   },
