610c770892ff01642f884fdebdbcd563bf48b274
Darius Rodzevic <35233347+DaRoTP@users.noreply.github.com>
2021-11-15 15:48:23 +0100

    Meter summary view (#42)
    * [MODIFY] #27 modifed summary view for Meter Poll
    
    * [ADD] #27 added percentage classes low, medium and high
    
    * [FIX] #27 fixed initial meter values when commingback to the voting panel

--- src/views/MeterPoll/MeterPollSummary.vue
+++ src/views/MeterPoll/MeterPollSummary.vue
@@ -1,70 +1,99 @@
 <template>
   <div class="flex flex-col gap-2">
-    <div
-      v-for="(option, index) in options"
-      :key="option.name"
-      class="bg-white shadow-lg rounded-sm overflow-hidden"
-    >
-      <div class="flex gap-2 relative">
-        <span
-          class="
-            text-white
-            p-1
-            bg-primary
-            absolute
-            rounded-md
-            ml-1
-            mt-1
-            shadow-md
-            text-xs
-          "
-          >{{ selectedOptions[index] }}%</span
+    <h1 class="text-primary font-bold text-xl">Your Results</h1>
+    <div class="flex flex-col gap-1">
+      <div
+        class="meter-poll-card"
+        v-for="({ option, val }, index) in sortedList"
+        :key="option.name"
+      >
+        <PollOption
+          :name="option.name"
+          :imageSrc="option.imageSrc"
+          :index="index + 1"
+          :showContentRight="true"
         >
-        <img
-          class="object-cover"
-          :style="{ width: '6rem' }"
-          :src="option.imageSrc"
-          alt=""
-        />
-        <span class="flex-1 flex-wrap">{{ option.name }}</span>
-      </div>
-      <div class="relative" :style="{ height: '0.25rem' }">
-        <span class="bg-gray-400 w-full h-full absolute left-0" />
-        <span
-          class="bg-primary absolute h-full left-0"
-          :style="{
-            width: `${choicePercentage(selectedOptions[index])}%`,
-          }"
-        />
+          <template #content-right>
+            <div
+              :class="[percetageClasses(val)]"
+              class="flex justify-center items-center w-full text-sm font-bold"
+            >
+              {{ val }}%
+            </div>
+          </template>
+        </PollOption>
+        <div
+          class="meter-bar"
+          :class="[percetageClasses(val)]"
+          :style="{ width: `${val}%` }"
+        ></div>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from "vue";
+import { defineComponent, computed, PropType } from "vue";
 import { IOption } from "@/types";
 import { Views } from "@/router/viewNames";
+import PollOption from "@/components/PollOptions/PollOption.vue";
 
 export default defineComponent({
   name: Views.POLL_SUMMARY.METER,
+  components: {
+    PollOption,
+  },
   props: {
     options: {
       type: Array as PropType<IOption[]>,
       required: true,
     },
-    selectedOptions: {
-      type: Array as PropType<string[]>,
+    submittingData: {
+      type: Object as PropType<Record<string, number>>,
       required: true,
     },
   },
-  setup() {
-    const choicePercentage = (number: number) => {
-      return number;
+  setup(props) {
+    const sortedList = computed(() => {
+      return props.options
+        .map((option) => ({ option, val: props.submittingData[option.name] }))
+        .sort((a, b) => b.val - a.val);
+    });
+
+    const percetageClasses = (percentage: number) => {
+      if (percentage < 40) return "low-color";
+      if (percentage < 70) return "medium-color";
+      else return "high-color";
     };
+
     return {
-      choicePercentage,
+      sortedList,
+      percetageClasses,
     };
   },
 });
 </script>
+<style lang="scss">
+.meter-poll-card {
+  position: relative;
+  .meter-bar {
+    height: 0.25rem;
+    width: 0;
+  }
+
+  .low-color {
+    background: rgb(201, 114, 103);
+    color: rgb(94, 32, 56);
+  }
+
+  .medium-color {
+    background: rgb(255, 202, 87);
+    color: rgb(95, 59, 11);
+  }
+
+  .high-color {
+    background: rgb(95, 182, 102);
+    color: rgb(227, 255, 229);
+  }
+}
+</style>
